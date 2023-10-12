c72cdedf00b249e1b28e38f8351d12496c85d8fc
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-10-12 22:00:40 +0900

    Fix: カードリストを全て表示

--- hangry-bahamut-cli/src/components/BaseHungryBahamuto.vue
+++ hangry-bahamut-cli/src/components/BaseHungryBahamuto.vue
@@ -1,11 +1,24 @@
 <template>
-  <v-container class="hello">
+  <v-container class="cardType">
     <v-row>
-      <v-col v-for="n in 12" :key="n" cols="2">
-        <div>
-          <img src="@/assets/cardList/01.harapekobahamuto.png.png" />
-        </div>
-      </v-col>
+      <div>
+        <img src="@/assets/cardList/01.harapekobahamuto.png" />
+        <img src="@/assets/cardList/02.kodomobahamuto.png" />
+        <img src="@/assets/cardList/03.hanekaesigoburin.png" />
+        <img src="@/assets/cardList/04.soratobunaihu.png" />
+        <img src="@/assets/cardList/05.owaka-re.png" />
+        <img src="@/assets/cardList/06.ideyoshi.png" />
+        <img src="@/assets/cardList/07.yomigae-ru.png" />
+        <img src="@/assets/cardList/08.irekae-ru.png" />
+        <img src="@/assets/cardList/09.kuronekonoshippo.png" />
+        <img src="@/assets/cardList/10.ginnekonoshippo.png" />
+        <img src="@/assets/cardList/11.karasunootsukai.png" />
+        <img src="@/assets/cardList/12.youseinomegane.png" />
+        <img src="@/assets/cardList/13.akumanohukiya.png" />
+        <img src="@/assets/cardList/14.hiramekisuisyou.png" />
+        <img src="@/assets/cardList/15.hoshihurusunadokei.png" />
+        <img src="@/assets/cardList/16.majonootodokemono.png" />
+      </div>
     </v-row>
   </v-container>
 </template>
@@ -15,7 +28,22 @@
 export default {
   data() {
     return {
-      imgSrc: require('@/assets/cardList/01.harapekobahamuto.png.png')
+      imgSrc: require('@/assets/cardList/01.harapekobahamuto.png'),
+      imgSrc: require('@/assets/cardList/02.kodomobahamuto.png'),
+      imgSrc: require('@/assets/cardList/03.hanekaesigoburin.png'),
+      imgSrc: require('@/assets/cardList/04.soratobunaihu.png'),
+      imgSrc: require('@/assets/cardList/05.owaka-re.png'),
+      imgSrc: require('@/assets/cardList/06.ideyoshi.png'),
+      imgSrc: require('@/assets/cardList/07.yomigae-ru.png'),
+      imgSrc: require('@/assets/cardList/08.irekae-ru.png'),
+      imgSrc: require('@/assets/cardList/09.kuronekonoshippo.png'),
+      imgSrc: require('@/assets/cardList/10.ginnekonoshippo.png'),
+      imgSrc: require('@/assets/cardList/11.karasunootsukai.png'),
+      imgSrc: require('@/assets/cardList/12.youseinomegane.png'),
+      imgSrc: require('@/assets/cardList/13.akumanohukiya.png'),
+      imgSrc: require('@/assets/cardList/14.hiramekisuisyou.png'),
+      imgSrc: require('@/assets/cardList/15.hoshihurusunadokei.png'),
+      imgSrc: require('@/assets/cardList/16.majonootodokemono.png')
     }
   }
 }
