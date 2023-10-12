9eb5a006a6661693bc51c858a814c117dcd7dd32
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-10-12 21:44:36 +0900

    Fix:カードリスト画像フォルダ作成/画像表示成功

--- hangry-bahamut-cli/.eslintrc.js
+++ hangry-bahamut-cli/.eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
+    "plugin:vue/vue3-essential",
+    "@vue/standard",
+    "plugin:prettier/recommended", // 追加
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: "@babel/eslint-parser",
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+  plugins: [
+    "vue",
+    "prettier", // 追加
+  ],
+  rules: {]
+     "prettier/prettier": "error", // 追加
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
--- hangry-bahamut-cli/src/components/BaseHungryBahamuto.vue
+++ hangry-bahamut-cli/src/components/BaseHungryBahamuto.vue
@@ -1,24 +1,22 @@
 <template>
   <v-container class="hello">
     <v-row>
-      <v-col
-        v-for="n in 12"
-        :key="n"
-        cols="2"
-      >
-        はらぺこバハムート{{ n }}
+      <v-col v-for="n in 12" :key="n" cols="2">
+        <div>
+          <img src="@/assets/cardList/01.harapekobahamuto.png.png" />
+        </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'BaseHungryBahamuto',
-  props: {
+  data() {
+    return {
+      imgSrc: require('@/assets/cardList/01.harapekobahamuto.png.png')
+    }
   }
 }
 </script>
-
-<style scoped>
-</style>
