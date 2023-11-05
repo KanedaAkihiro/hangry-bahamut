6c63b94c9ebf66f0a5074bf3fa471a9ba33dd7c0
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-11-05 14:24:14 +0900

    Add: カードリストコンポーネント作成/ルーティング済み

--- hangry-bahamut-cli/src/components/BaseHungryBahamuto.vue
+++ hangry-bahamut-cli/src/components/BaseHungryBahamuto.vue
@@ -1,24 +1,42 @@
 <template>
-  <v-container class="hello">
+  <v-container class="cardType">
     <v-row>
-      <v-col
-        v-for="n in 12"
-        :key="n"
-        cols="2"
-      >
-        はらぺこバハムート{{ n }}
+      <!-- img変数を作成し、imgListないの画像目（文字列）を格納し繰り返し表示 -->
+      <v-col v-for="img in imgList" :key="img">
+        <!--
+        ・'@/assets/'という文字列＋img(カード名)+'.png'という文字列を結合し、requireでアセット内のリソースに該当のパスと合致した画像を要求
+        ・つまリ表示させたい「パスを作成」→requireでアセットに要求→V-forで取得したパスの画像を繰り返し表示
+        -->
+        <img :src="require('@/assets/' + img + '.png')" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'BaseHungryBahamuto',
-  props: {
+  data() {
+    return {
+      imgList: [
+        '01.harapekobahamuto',
+        '02.kodomobahamuto',
+        '03.hanekaesigoburin',
+        '04.soratobunaihu',
+        '05.owaka-re',
+        '06.ideyoshi',
+        '07.yomigae-ru',
+        '08.irekae-ru',
+        '09.kuronekonoshippo',
+        '10.ginnekonoshippo',
+        '11.karasunootsukai',
+        '12.youseinomegane',
+        '13.akumanohukiya',
+        '14.hiramekisuisyou',
+        '15.hoshihurusunadokei',
+        '16.majonootodokemono'
+      ]
+    }
   }
 }
 </script>
-
-<style scoped>
-</style>
---
+++ hangry-bahamut-cli/src/components/cardSpace.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="cardSpace">
+    <v-row>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
